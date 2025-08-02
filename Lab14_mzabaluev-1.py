24fbe3cae9efa130d6b71c6afee5b1461d605b48
Sauz-Rx <awesen561@gmail.com>
2025-08-02 13:13:02 -0400

    Lab 14 Completec! Comply changed up! Droped cosine, instead using x + 10 * 2

--- Lab14_mzabaluev-1.py
+++ Lab14_mzabaluev-1.py
@@ -2,20 +2,25 @@
 Cosine function
 Maksim Zabaluev 
 8/2/2025
-Plot a math formula of our choosing. I chose: cos(x) 
+Plot a math formula of our choosing. I chose: x + 10 * 2
 '''
-import math
+
 import matplotlib.pyplot as plt 
 
+x_val = []
+y_val = []
 
-x = list(range(0,360))
+for i in range(10):
+    x_val.append(i)
+    y_val.append(i)
 
-# calculate the cosine for each x value  
-y = [math.cos(math.radians(a)) for a in x]
+x_val.append(10)
+y_val.append(y_val[-1]* 2)
+
 
 plt.figure()
-plt.plot(x, y, label ='cos(x)', color = 'blue', linewidth = 2)
-plt.xlabel('degrees')
+plt.plot(x_val, y_val, label ='x + 10 *2', color = 'red', linewidth = 3)    
+plt.xlabel('x')
 plt.ylabel('y')
 plt.grid(True)
 plt.show()
